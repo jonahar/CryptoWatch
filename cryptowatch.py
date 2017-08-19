3f4cb647b1b0ebf10be101f6aa41bd4def445daf
jonahar <jonahar@gmail.com>
2017-08-20 00:14:10 +0300

    removed unnecessary refresh

--- cryptowatch.py
+++ cryptowatch.py
@@ -8,20 +8,21 @@ import wallet as Wallet
 def run_crypto_watch(stdscr):
     renderer.init_render(stdscr)
     wallet = Wallet.Wallet()
-    option = 0
-    while True:
-        coins = wallet.get_balances()
+    option = renderer.ADD
+    refresh = True
+    while option != renderer.EXIT:
+        if refresh:
+            coins = wallet.get_balances()
+            refresh = False
         option = renderer.display_main_scr(stdscr, coins, option=option)
         if option == renderer.ADD:
             renderer.display_add_scr(stdscr, wallet)
         elif option == renderer.MANAGE_COIN:
             renderer.manage_scr(stdscr, wallet)
         elif option == renderer.REFRESH:
-            pass  # coins will be refreshed in the next loop cycle
+            refresh = True
         elif option == renderer.DELETE_WALLET:
             wallet.delete()
-        elif option == renderer.EXIT:
-            return
 
 
 # wrapper will initialize the screen, turn off keys echoing, turn on
