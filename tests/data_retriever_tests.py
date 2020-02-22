e88bf05587c36c6892c1d14d4f20649c3c1f2e6b
Jona Harris <jonahar@gmail.com>
2020-02-22 12:45:54 +0200

    test two lookup cases: single address and multiple addresses

--- tests/data_retriever_tests.py
+++ tests/data_retriever_tests.py
@@ -4,7 +4,16 @@ from data_retriever import NOT_FOUND, lookup_addresses
 
 
 class DataRetrieverTest(unittest.TestCase):
-    def test_btc_lookup(self):
+    
+    def test_btc_lookup_single(self):
+        addresses = [
+            "1FDMwEo8qNa9icVcooBUoGvA6NriePtJJ3",  # balance 50
+        ]
+        expected_balances = [50]
+        balances = lookup_addresses(coin="BTC", addresses=addresses)
+        self.assertListEqual(balances, expected_balances)
+    
+    def test_btc_lookup_multiple(self):
         # both addresses were last used in 2009. probably wouldn't change.
         # worst case, test will fail and we'll figure it out
         addresses = [
@@ -15,7 +24,15 @@ class DataRetrieverTest(unittest.TestCase):
         balances = lookup_addresses(coin="BTC", addresses=addresses)
         self.assertListEqual(balances, expected_balances)
     
-    def test_eth_lookup(self):
+    def test_eth_lookup_single(self):
+        addresses = [
+            "0x2C3C1dF3c25FE3875cE559FC68c6E9068bD952ED",  # balance 0.000518161526620852
+        ]
+        expected_balances = [0.000518161526620852]
+        balances = lookup_addresses(coin="ETH", addresses=addresses)
+        self.assertListEqual(balances, expected_balances)
+    
+    def test_eth_lookup_multiple(self):
         addresses = [
             "0xDf190dC7190dfba737d7777a163445b7Fff16133",  # balance 0
             "0x2C3C1dF3c25FE3875cE559FC68c6E9068bD952ED",  # balance 0.000518161526620852
@@ -25,7 +42,15 @@ class DataRetrieverTest(unittest.TestCase):
         balances = lookup_addresses(coin="ETH", addresses=addresses)
         self.assertListEqual(balances, expected_balances)
     
-    def test_bch_lookup(self):
+    def test_bch_lookup_single(self):
+        addresses = [
+            "1FDMwEo8qNa9icVcooBUoGvA6NriePtJJ3",  # balance 50
+        ]
+        expected_balances = [50]
+        balances = lookup_addresses(coin="BCH", addresses=addresses)
+        self.assertListEqual(balances, expected_balances)
+    
+    def test_bch_lookup_multiple(self):
         addresses = [
             "15NUwyBYrZcnUgTagsm1A7M2yL2GntpuaZ",  # balance 0
             "1FDMwEo8qNa9icVcooBUoGvA6NriePtJJ3",  # balance 50
